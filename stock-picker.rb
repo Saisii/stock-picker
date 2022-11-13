df237216b65b45decfc21ddb4795b1d0e3a14054
Saisii <sethconger@gmail.com>
2022-11-13 16:37:23 +0300

    clean up code

--- stock-picker.rb
+++ stock-picker.rb
@@ -5,10 +5,8 @@ def stock_picker(stock_array)
 
     stock_array.each_with_index do |element, index_forward|
 
-    
 
         stock_array.reverse.each_with_index do |r_element, index_reverse|
-
 
             buy = 0
             sell = 0
@@ -16,34 +14,18 @@ def stock_picker(stock_array)
 
             if index_forward < stock_array.length - index_reverse  -1
 
-
                 buy = element
                 sell = r_element
                 profit = sell - buy
 
-                # p "element is #{element} and buy is #{buy}. Should be the same"
-                # p "r_element is #{r_element} and sell is #{sell}. Should be the same"
-                # p "forward index: #{index_forward}, reverse index: #{(stock_array.length - index_reverse - 1)}"
-                # p "buy at #{buy} and sell at #{sell}" 
-                # p "You will make #{profit}$"
-
                 if profit > best_profit
-                    # p "#{profit} is greater than #{higheset_trade_counter}"
                     best_profit = profit
                     best_profit_array = [index_forward, stock_array.length - index_reverse - 1]
-                    
-                    # p "highest_trade_counter has been updated: #{best_profit}"
-                    # p "at index of #{best_profit_array}"
-                    # puts ""
+                        
                 end
-            end
-
-          
+            end          
         end
     end
-
-    # p "best profit is #{best_profit}"
-
 
     best_profit_array
 end
@@ -59,4 +41,4 @@ end
 
 
 
-p stock_picker([1,3,6,9,15,8,60,1,10])
+p stock_picker([90,3,6,3,15,8,60,1,0])
